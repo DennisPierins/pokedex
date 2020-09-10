c6e6495d0e023741654967e6e41991cfcf8093b2
DennisPierins <dennispierins@gmail.com>
2020-09-10 11:36:35 +0200

    Added 'for' loop and 'if, else' conditional
    The loop will display a list of all defined pokemon. Pokemon with a height higher than 1 receive an extra message

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,11 @@ let pokemonList = [
     type: ['Grass', 'Poison']
   }
 ];
+
+// Displays all pokemon with their respected heights; pokemon with heights higher than 1 display an extra message
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].height > 1){
+    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " - Wow, that's big!" + "<br>" + "<br>")
+} else {document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + "<br>" + "<br>")
+}
+}
