7f3c1c7bda03b1bed707c7c961af4b42d6b10841
DennisPierins <dennispierins@gmail.com>
2020-09-18 16:37:21 +0200

    Indentation corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,56 @@
 let pokemonRepository = (function () {
   let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['Grass', 'Poison']
-  },
-  {
-    name: 'Ivysaur',
-    height: 1,
-    type: ['Grass', 'Poison']
-  },
-  {
-    name: 'Venusaur',
-    height: 2,
-    type: ['Grass', 'Poison']
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['Grass', 'Poison']
+    },
+    {
+      name: 'Ivysaur',
+      height: 1,
+      type: ['Grass', 'Poison']
+    },
+    {
+      name: 'Venusaur',
+      height: 2,
+      type: ['Grass', 'Poison']
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
 
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-function getAll() {
-  return pokemonList;
-}
+  //Creates a list with buttons
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemonlist");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    //Makes the buttons clickable and shows the pokemon's name in console
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon)
+    })
+  }
 
-//Creates a list with buttons
-function addListItem(pokemon) {
-  let pokemonList = document.querySelector(".pokemonlist");
-  let listItem = document.createElement("li");
-  let button = document.createElement("button");
-  button.innerText = pokemon.name;
-  button.classList.add("button-class");
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-//Makes the buttons clickable and shows the pokemon's name in console
-  button.addEventListener('click', function (event) {
-    showDetails(pokemon)
-  })
-}
+  //Shows the pokemon's name in console
+  function showDetails(pokemon) {
+    console.log(pokemon.name)
+  }
 
-//Shows the pokemon's name in console
-function showDetails(pokemon) {
-  console.log(pokemon.name)
-}
-
-return {
-  add: add,
-  getAll: getAll,
-  addListItem: addListItem,
-  showDetails: showDetails
-};
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
+  };
 })();
 
 //Calls a function that displays a list with buttons for each pokemon name
